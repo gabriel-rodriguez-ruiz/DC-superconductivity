f9cf5d9f0f343f267789a4b7ffbdc48643552194
Gabriel <gabrielrr96@gmail.com>
2024-03-08 16:04:33 -0300

    Added Omega dependence.

--- Serbyn_without_Nambu.py
+++ Serbyn_without_Nambu.py
@@ -11,7 +11,7 @@ from pauli_matrices import tau_0, sigma_0, tau_z, sigma_x, sigma_y, tau_y
 import scipy
 import matplotlib.pyplot as plt
 
-def G_k(k, epsilon_n, w_0, Gamma, mu):
+def G_k(k, epsilon_n, w_0, Gamma, mu, Omega):
     r""" Matsubara Green's function.
     
     .. math ::
@@ -20,19 +20,19 @@ def G_k(k, epsilon_n, w_0, Gamma, mu):
         \epsilon_n=2\pi/\beta\left(n+\frac{1}{2}\right)
     """
     h_0 = 2*w_0*np.cos(k)-mu
-    return 1/(1j*epsilon_n+1j*np.sign(epsilon_n)*Gamma-h_0)
+    return 1/(1j*epsilon_n+1j*np.sign(epsilon_n)*Gamma+1j*Omega-h_0)
 
 def G_frozen(k, omega, w_0, Gamma, mu):
     h_0 = 2*w_0*np.cos(k)-mu
     return 1/(omega+1j*Gamma-h_0)    
 
-def get_Q_k(k, w_0, Gamma, mu, N, beta):
+def get_Q_k(k, w_0, Gamma, mu, N, beta, Omega):
     epsilon_n = np.pi/beta*(2*np.arange(-N, N) + 1)
     sumand = np.zeros_like(epsilon_n, dtype=complex)
     for i in range(len(epsilon_n)):
         v = -2*w_0*np.sin(k)
-        sumand[i] = v**2*G_k(k, epsilon_n[i], w_0, Gamma, mu)**2
-    return -1/(2*beta)*np.sum(sumand, dtype=complex)
+        sumand[i] = v**2*G_k(k, epsilon_n[i], w_0, Gamma, mu, Omega)*G_k(k, epsilon_n[i], w_0, Gamma, mu, Omega=0)
+    return -1/(beta)*np.sum(sumand, dtype=complex)
 
 def Rho_k(k, omega, w_0, Gamma, mu):
     h_0 = 2*w_0*np.cos(k)-mu
@@ -88,14 +88,14 @@ def get_Q_k_Matsubara(k, omega, w_0, Gamma, mu, N, beta):
 
 #%%
 
-beta = 10
+beta = 500
 N = 100000
 Gamma = 0.01
 mu = 0
 w_0 = 1
-omega = np.linspace(-1, 1, 1000)
+omega = np.linspace(-0.01, 0.01, 1000)
 epsilon_n = np.pi/beta*(2*np.arange(-N, N) + 1)
-
+Omega = np.pi/beta*(2*2 + 1)
 # k = np.pi/2
 # Q_k = get_Q_k(k, w_0, Gamma, mu, N, beta)
 # sigma_quad_k = get_sigma_quad_k(k, w_0, Gamma, mu, beta)
@@ -103,15 +103,15 @@ epsilon_n = np.pi/beta*(2*np.arange(-N, N) + 1)
 
 L = 200
 k = 2*np.pi/L*np.arange(0, L)
-Q_k = [get_Q_k(k, w_0, Gamma, mu, N, beta) for k in k]
+Q_k = [1/L*get_Q_k(k, w_0, Gamma, mu, N, beta, Omega) for k in k]
 # Q_k_Matsubara = [get_Q_k_Matsubara(k, omega, w_0, Gamma, mu, N, beta) for k in k]
-sigma_k = [get_sigma_k(k, omega, w_0, Gamma, mu, beta) for k in k]
+sigma_k = [1/L*get_sigma_k(k, omega, w_0, Gamma, mu, beta) for k in k]
 # sigma_quad_k = [get_sigma_quad_k(k, w_0, Gamma, mu, beta) for k in k]
 # zero_order_k = [get_zero_order_k(k, omega, w_0, Gamma, mu, beta) for k in k]
 fig, ax = plt.subplots()
 # ax.plot(k, Q_k, label="Q")
 ax.plot(k, sigma_k, label=r"$\sigma$")
-ax.plot(k, 1/Gamma*np.array(Q_k), label="Q/Gamma")
+ax.plot(k, 1/Omega*np.array(Q_k), label="Q/Gamma")
 ax.set_xlabel("k")  
 plt.legend()
 
