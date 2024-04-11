7fd66072685666b76a147cebf53ca1a4e958bd5e
Gabriel F. Rodríguez Ruiz <gabrielrr96@gmail.com>
2024-04-11 08:36:49 -0300

    Normal density.

--- fermi_surface.py
+++ fermi_surface.py
@@ -43,12 +43,13 @@ def get_DOS(omega, eta, L_x, L_y, w_0, mu, B_x, B_y, Lambda):
     k_x_values = 2*np.pi/L_x*np.arange(0, L_x)
     k_y_values = 2*np.pi/L_y*np.arange(0, L_y)
     G = get_Green_function(omega+1j*eta, k_x_values, k_y_values, w_0, B_x, B_y, Lambda)
-    return 1/(L_x*L_y) * 1/np.pi*np.sum(-np.trace(np.imag(G)))
+    return 1/(L_x*L_y) * (-1)/np.pi*np.sum(np.imag(G), axis=(0,1))
 
 def get_normal_density(omega_values, eta, L_x, L_y, w_0, mu, B_x, B_y, Lambda):
     DOS = np.zeros(len(omega_values))
     for i, omega in enumerate(omega_values):
-        DOS[i] = get_DOS(omega, eta, L_x, L_y, w_0, mu, B_x, B_y, Lambda)
+        DOS[i] = np.trace(get_DOS(omega, eta, L_x, L_y, w_0, mu, B_x, B_y, Lambda))
+        print(i)
     return np.sum(DOS)
     
 #%%
@@ -87,7 +88,7 @@ B = 0
 B_x = B * np.cos(theta)
 B_y = B * np.sin(theta)
 Lambda = 0.56
-omega_values = np.linspace(-10+mu, mu, 100)
+omega_values = np.linspace(-10+mu, mu, 50)
 eta = 0.1
 
 DOS = np.zeros(len(omega_values))
@@ -102,7 +103,7 @@ ax.plot(omega_values, DOS)
 ax.set_ylabel(r"$\rho(\omega)$")
 ax.set_xlabel(r"$\omega$")
 
-#%%
+#%% Normal density vs. By
 L_x = 200
 L_y = 200
 w_0 = 10
@@ -114,7 +115,7 @@ Lambda = 0.56#5*Delta/np.sqrt((4*w_0 + mu)/w_0)/2
 h = 1e-2
 k_x_values = 2*np.pi/L_x*np.arange(0, L_x)
 k_y_values = 2*np.pi/L_y*np.arange(0, L_y)
-omega_values = np.linspace(-10+mu, mu, 100)
+omega_values = np.linspace(-10+mu, mu, 50)
 eta = 0.1
 
 n_B_y = np.zeros(len(B_values))
@@ -123,7 +124,7 @@ for i, B in enumerate(B_values):
     B_x = B * np.cos(theta)
     B_y = B * np.sin(theta)
     n_B_y[i] = get_normal_density(omega_values, eta, L_x, L_y, w_0, mu, B_x, B_y, Lambda)
-    print(i)
+    print(n_B_y)
 
 fig, ax = plt.subplots()
 ax.plot(B_values, n_B_y, "-o")
