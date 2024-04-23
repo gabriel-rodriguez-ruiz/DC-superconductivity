effc5e91ddfb8af6a0576956a9321bd7e20ff8c4
Gabriel <gabrielrr96@gmail.com>
2024-04-23 14:16:19 -0300

    I forgot to commit.

--- twisted_boundary_conditions.py
+++ twisted_boundary_conditions.py
@@ -131,6 +131,7 @@ Lambda = 0.56
 h = 1e-2
 k_x_values = 2*np.pi/L_x*np.arange(0, L_x)
 k_y_values = 2*np.pi/L_y*np.arange(0, L_y)
+n = 0.136
 
 n_theta = np.zeros((len(theta_values), 3))
 for i, theta in enumerate(theta_values):
@@ -140,8 +141,8 @@ for i, theta in enumerate(theta_values):
     print(i)
 
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-ax.plot(theta_values, n_theta[:,0], "or", label=r"$n_{xx}$")
-ax.plot(theta_values, n_theta[:,1], "ob", label=r"$n_{yy}$")
+ax.plot(theta_values, n_theta[:,0]/n, "or", label=r"$n_{xx}$")
+ax.plot(theta_values, n_theta[:,1]/n, "ob", label=r"$n_{yy}$")
 # ax.plot(theta_values, np.sqrt(n_theta[:,0]**2+n_theta[:,1]**2), "og", label=r"$|n_s|^2$")
 # ax.plot(theta_values+np.pi/2, n_theta[:,0], "or", label=r"$n_{s,\perp}$")
 # ax.plot(theta_values+np.pi/2, n_theta[:,1], "ob", label=r"$n_{s,\parallel}$")
@@ -160,7 +161,7 @@ ax.legend()
 plt.tight_layout()
 
 #%%
-np.savez("n_angle", n_theta=n_theta, theta_values=theta_values,
+np.savez("n_angle_mu=-32_L=400", n_theta=n_theta, theta_values=theta_values,
          B=B)
 #%% Load data
 
@@ -225,7 +226,7 @@ data = np.load(file_to_open)
 n_B_y = data["n_B_y"]
 # Data_n = np.load(data_folder / "n_mu_-40_L=400.npz")  #does not change approx. with magnetic field
 Data_n = np.load(data_folder / "n_mu_-32_L=400.npz")  #does not change approx. with magnetic field
-n = Data_n["n"]
+n = Data_n["n"] #n=0.136
 
 B_values = data["B_values"]
 Delta = data["Delta"]
