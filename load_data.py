c5683f86b3555c6d608258fdc74e21709796c7c5
Gabriel <gabrielrr96@gmail.com>
2025-03-30 11:29:05 -0300

    I could fit parallel superfluid density.

--- load_data.py
+++ load_data.py
@@ -32,6 +32,8 @@ ax.legend()
 plt.show()
 
 #%%
+data_folder = Path("Data/")
+
 file_path = data_folder / 'n_s_5_7GHz.dat'
 
 data = pd.read_table(file_path, dtype=float,
@@ -52,7 +54,7 @@ ax.legend()
 
 plt.show()
 
-#%%
+#%% Looking cuadratic dependence
 fig, ax = plt.subplots()
 
 ax.scatter(data["field 0°"][:]**2, data["n_s 0°"][:],
@@ -64,10 +66,10 @@ ax.plot(data["field 0°"][:8]**2, f(data["field 0°"][:8]**2), "b--")
 
 ax.scatter(data["field 90°"][:]**2, data["n_s 90°"][:],
            label=r"$n_s(90°)$", color="black")
-m, n = np.polyfit(data["field 90°"][:8]**2,
-                           data["n_s 90°"][:8], deg=1)
+m, n = np.polyfit(data["field 90°"][:3]**2,
+                           data["n_s 90°"][:3], deg=1)
 h = lambda x: m * x + n
-ax.plot(data["field 90°"][:8]**2, h(data["field 90°"][:8]**2), "--")
+ax.plot(data["field 90°"][:3]**2, h(data["field 90°"][:3]**2), "--")
 
 ax.set_title("5.7 GHz Resonator, 0°")
 ax.set_xlabel(r"$B^2$ [$T^2$]")
@@ -77,69 +79,104 @@ ax.legend()
 
 plt.show()
 
-#%%
-# fig, ax = plt.subplots()
-# data.plot(x=0, ax=ax, marker="o", markersize=2)
+#%% Interpolation to theory
 
-ax.set_xlabel(r"$B$ [$T$]")
-ax.set_ylabel(r"$\Delta f [Hz]$")
-ax.scatter(data["B"][50]**2, data[r"$\Delta f_{\perp}(3.7 GHz)$"][50])
-a, b = np.polyfit(data["B"][:50]**2,
-                              data[r"$\Delta f_{\perp}(3.7 GHz)$"][:50]
-                              , deg=1)
-f = lambda x: a * x + b
-ax.plot(data["B"][:100]**2, f(data["B"][:100]**2), "--")
-m, n = np.polyfit(data["B"][50:259]**2,
-                              data[r"$\Delta f_{\perp}(3.7 GHz)$"][50:259]
-                              , deg=1)
-h = lambda x: m * x + n
-ax.plot(data["B"][50:259]**2, h(data["B"][50:259]**2), "--")
+data_folder = Path("../Density-of-states/Data/")
+# data_folder = Path("../anisotropic-superfluid/Data/")
 
-ax.scatter(data["B"][100]**2, data[r"$\Delta f_{\parallel}(3.7 GHz)$"][100])
-c, d = np.polyfit(data["B"][:100]**2,
-                              data[r"$\Delta f_{\parallel}(3.7 GHz)$"][:100]
-                              , deg=1)
-g = lambda x: c * x + d
-ax.plot(data["B"][:200]**2, g(data["B"][:200]**2), "--")
-o, p = np.polyfit(data["B"][100:]**2,
-                              data[r"$\Delta f_{\parallel}(3.7 GHz)$"][100:]
-                              , deg=1)
-j = lambda x: o * x + p
-ax.plot(data["B"][100:]**2, j(data["B"][100:]**2), "--")
+file_to_open = data_folder / "n_By_mu_-39_L=1000_h=0.01_B_y_in_(0.0-1.2)_Delta=0.2_lambda=0.56.npz"
+# file_to_open = data_folder / "n_By_mu_-39_L=1000_h=0.01_B_y_in_(0.0-0.4)_Delta=0.2_lambda_R=0.56_lambda_D=0_g_xx=2_g_xy=0_g_yy=1_theta=0_points=24.npz"
 
-fig.text(0.3, 0.8, r"$B^*_{\perp}=$"+f"{np.round(data['B'][50],3)}T")
-fig.text(0.3, 0.7, r"$B^*_{\parallel}=$"+f"{np.round(data['B'][100],3)}T")
-
-#%%fitting
-
-data_folder = Path("../AC-superconductivity/Data/")
-file_to_open = data_folder / "Response_kernel_vs_B_mu=-39_L=100_Gamma=0.1_Omega=0.npz"
 Data = np.load(file_to_open)
 
-K = Data["K"]
+n_B_y = Data["n_B_y"]
 B_values = Data["B_values"]
 Lambda = Data["Lambda"]
+# Lambda_R = Data["Lambda_R"]
+# Lambda_D = Data["Lambda_D"]
 Delta = Data["Delta"]
 theta = Data["theta"]
 w_0 = Data["w_0"]
 mu = Data["mu"]
-part = Data["part"]
-Omega = Data["Omega"]
-Gamma = Data["Gamma"]
-# L_x = Data["L_x"]
-# L_y = Data["L_y"]
+L_x = Data["L_x"]
+h = Data["h"]
+# g_xx = Data["g_xx"]
+# g_yy = Data["g_yy"]
+# g_xy = Data["g_xy"]
+# g_yx = Data["g_yx"]
+
+def interpolation_for_theory(x):
+    return [
+            np.interp(x, B_values/Delta, n_B_y[:, 0]),
+            np.interp(x, B_values/Delta, n_B_y[:, 1])
+            ]
 
 
-f_geo = np.array([3.7e9, 4.4e9])
-f_kin_perp = np.sqrt(K[:, 0, 0]/K[0, 0, 0])
-f_kin_para = np.sqrt(K[:, 1, 0]/K[0, 1, 0])
-Delta_f_perp = 1e8 * np.array([f_geo * f_kin / np.sqrt(f_geo**2 + f_kin**2) for f_kin in f_kin_perp])
-Delta_f_para = 1e8 * np.array([f_geo * f_kin / np.sqrt(f_geo**2 + f_kin**2) for f_kin in f_kin_para])
 
 fig, ax = plt.subplots()
-ax.plot(B_values**2, f_kin_perp, "-o",  label=r"$\perp$")
-ax.plot(B_values**2, f_kin_para, "-o",  label=r"$\parallel$")
-ax.set_xlabel(r"$B^2$")
-ax.set_ylabel(r"$\sqrt{K}$")
+ax.plot(B_values/Delta, n_B_y[:, 0], "-sk",  label=r"$n_s(90°)$")
+ax.plot(B_values/Delta, n_B_y[:, 1], "-sr",  label=r"$n_s(0°)$")
+ax.plot(B_values/Delta, interpolation_for_theory(B_values/Delta)[0])
+ax.plot(B_values/Delta, interpolation_for_theory(B_values/Delta)[1])
+
+
+ax.set_xlabel(r"$B$")
+ax.set_ylabel(r"$n_s$")
 ax.legend()
-# ax.plot(B_values**2, Delta_f_para[:,1]-1e8, "-o",  label=r"$K^{(L)}_{xx}(\Omega=$"+f"{Omega}"+r"$,\mu=$"+f"{np.round(mu,2)}"+r", $\lambda=$"+f"{Lambda})")
+
+
+
+#%% Fitting to experiment
+from scipy.optimize import curve_fit
+
+
+data_folder = Path("Data/")
+
+file_path = data_folder / 'n_s_5_7GHz.dat'
+
+data = pd.read_table(file_path, dtype=float,
+                     header=0, sep='\t\t',
+                     names=["field 0°", "n_s 0°", "n_s_error 0°", "field 90°",
+                            "n_s 90°", "n_s_error 90°"],
+                     engine='python')
+
+# data = data.replace('nan', np.nan)   #replace nan to np.nan
+# data = data.dropna()  #remove NaN
+# def model_parallel(x, a, b, c, d):
+#     return a*(interpolation_for_theory(b*x)[1]) + c + d * x**2
+
+def model_parallel(x, a, b, c):
+    return a*(interpolation_for_theory(x)[1]) + b + c* x**2
+
+def model_perpendicular(x, a, b, c):
+    return a*(interpolation_for_theory(x)[0]) + b + c* x**2
+
+# initial_parameters = [ 1.55679010e+06, -9.66768578e+03,  2.13135475e+07, -1.09383906e+06]
+initial_parameters_parallel = [ 2.10435188e+06,  2.13288010e+07, -1.26647832e+03]
+popt_parallel, pcov_parallel = curve_fit(model_parallel, data["field 0°"]/data["field 0°"][7], data["n_s 0°"],
+                                          p0=initial_parameters_parallel)
+
+
+initial_parameters_perpendicular = [ 1.36837991e+07,  2.10537826e+07, -1.03067549e+03]
+popt_perpendicular, pcov_perpendicular = curve_fit(
+                                                   model_perpendicular, data["field 90°"].dropna()/data["field 90°"][7], data["n_s 90°"].dropna(),
+                                                   p0=initial_parameters_perpendicular
+                                                   )
+
+fig, ax = plt.subplots()
+ax.errorbar(data["field 0°"]/data["field 0°"][7], data["n_s 0°"], yerr=data["n_s_error 0°"], label=r"$n_s(0°)$", color="red", fmt="o")
+ax.errorbar(data["field 90°"].dropna()/data["field 90°"][7], data["n_s 90°"].dropna(), yerr=data["n_s_error 90°"].dropna(), label=r"$n_s(90°)$", color="black", fmt="o")
+
+
+x_model_parallel  = data["field 0°"]/data["field 0°"][7]
+# fig, ax = plt.subplots()
+ax.plot(x_model_parallel, model_parallel(x_model_parallel, *popt_parallel), "-b",  label=r"fit of $n_s(0°)$")
+
+x_model_perpendicular  = data["field 90°"].dropna()/data["field 90°"][7]
+ax.plot(x_model_perpendicular, model_perpendicular(x_model_perpendicular, *popt_perpendicular), "-b",  label=r"fit of $n_s(90°)$")
+
+
+
+ax.set_xlabel(r"$B/\Delta_0$")
+ax.set_ylabel(r"$n_s$")
+ax.legend()
