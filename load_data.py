dc49f57035479691003616d568f935103a033253
Gabriel <gabrielrr96@gmail.com>
2025-04-04 17:03:21 -0300

    Travel to Baires.

--- load_data.py
+++ load_data.py
@@ -11,6 +11,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#%%
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+#%%
+
 data_folder = Path("Data/")
 
 file_path = data_folder / 'n_s_4_9GHz.dat'
@@ -84,15 +92,15 @@ plt.show()
 data_folder = Path("../Density-of-states/Data/")
 # data_folder = Path("../anisotropic-superfluid/Data/")
 
-file_to_open = data_folder / "n_By_mu_-39_L=1000_h=0.01_B_y_in_(0.0-1.2)_Delta=0.2_lambda=0.56.npz"
-# file_to_open = data_folder / "n_By_mu_-39_L=1000_h=0.01_B_y_in_(0.0-0.4)_Delta=0.2_lambda_R=0.56_lambda_D=0_g_xx=2_g_xy=0_g_yy=1_theta=0_points=24.npz"
+file_to_open = data_folder / "n_By_mu_-349.0_L=1500_h=0.001_B_y_in_(0.0-1.2)_Delta=0.2_lambda_R=2.435064935064935_lambda_D=0_g_xx=1_g_xy=0_g_yy=1_g_yx=0_theta=1.57_points=24.npz"
+# file_to_open = data_folder / "n_By_mu_-30_L=1000_h=0.01_B_y_in_(0.0-1.2)_Delta=0.2_lambda_R=0.56_lambda_D=0_g_xx=1_g_xy=0_g_yy=1_g_yx=0_theta=1.57_points=24.npz"
 
 Data = np.load(file_to_open)
 
 n_B_y = Data["n_B_y"]
 B_values = Data["B_values"]
-Lambda = Data["Lambda"]
-# Lambda_R = Data["Lambda_R"]
+# Lambda = Data["Lambda"]
+Lambda_R = Data["Lambda_R"]
 # Lambda_D = Data["Lambda_D"]
 Delta = Data["Delta"]
 theta = Data["theta"]
@@ -100,6 +108,7 @@ w_0 = Data["w_0"]
 mu = Data["mu"]
 L_x = Data["L_x"]
 h = Data["h"]
+# points = Data["points"]
 # g_xx = Data["g_xx"]
 # g_yy = Data["g_yy"]
 # g_xy = Data["g_xy"]
@@ -115,15 +124,26 @@ def interpolation_for_theory(x):
 
 fig, ax = plt.subplots()
 ax.plot(B_values/Delta, n_B_y[:, 0], "-sk",  label=r"$n_s(90°)$")
-ax.plot(B_values/Delta, n_B_y[:, 1], "-sr",  label=r"$n_s(0°)$")
+ax.plot(B_values/Delta, n_B_y[:, 1], "-or",  label=r"$n_s(0°)$")
 ax.plot(B_values/Delta, interpolation_for_theory(B_values/Delta)[0])
 ax.plot(B_values/Delta, interpolation_for_theory(B_values/Delta)[1])
 
-
+ax.set_title(r"$\lambda_R=$" + f"{np.round(Lambda_R,2)}"
+             +r"; $\Delta=$" + f"{Delta}"
+             +r"; $\theta=$" + f"{np.round(theta,2)}"
+             + r"; $\mu$"+f"={mu}"
+             +r"; $w_0$"+f"={w_0}"
+             +r"; $L_x=$"+f"{L_x}"
+             +f"; h={h}")
 ax.set_xlabel(r"$B$")
 ax.set_ylabel(r"$n_s$")
 ax.legend()
+saving_folder = Path("/home/gabriel/Dropbox/Figures/Fitting to superfluid density")
+file_name = f'interpolation_to_theory_lambda_R={np.round(Lambda_R,2)}_Delta={Delta}_' + \
+            f'mu={mu}_L={L_x}_h={h}_B_y_in_({np.min(B_values)}-{np.round(np.max(B_values),3)})_theta={np.round(theta,2)}.png'
 
+file_path = saving_folder / file_name
+plt.savefig(file_path, dpi=300)
 
 
 #%% Fitting to experiment
@@ -153,13 +173,12 @@ def model_perpendicular(x, a, c):
     # return a*(interpolation_for_theory(x)[0]) + b + c* x**2
     return a*(interpolation_for_theory(x)[0] - interpolation_for_theory(0)[0])  + data["n_s 90°"].dropna()[0] + c* x**2
 
-# initial_parameters = [ 1.55679010e+06, -9.66768578e+03,  2.13135475e+07, -1.09383906e+06]
 initial_parameters_parallel = [ 2.10435188e+06, -1.26647832e+03]
 popt_parallel, pcov_parallel = curve_fit(model_parallel, data["field 0°"]/data["field 0°"][8], data["n_s 0°"],
                                           p0=initial_parameters_parallel)
 
 
-initial_parameters_perpendicular = [ 1.95898403e+07, -7.99583256e+02]
+initial_parameters_perpendicular = [ 5.39729364e+06, -8.91725182e+02]
 popt_perpendicular, pcov_perpendicular = curve_fit(
                                                    model_perpendicular, data["field 90°"].dropna()/data["field 90°"][8], data["n_s 90°"].dropna(),
                                                    p0=initial_parameters_perpendicular
@@ -167,7 +186,7 @@ popt_perpendicular, pcov_perpendicular = curve_fit(
 
 fig, ax = plt.subplots()
 ax.errorbar(data["field 0°"]/data["field 0°"][8], data["n_s 0°"], yerr=data["n_s_error 0°"], label=r"$n_s(0°)$", color="red", fmt="o", zorder=1)
-ax.errorbar(data["field 90°"].dropna()/data["field 90°"][8], data["n_s 90°"].dropna(), yerr=data["n_s_error 90°"].dropna(), label=r"$n_s(90°)$", color="black", fmt="o", zorder=2)
+ax.errorbar(data["field 90°"].dropna()/data["field 90°"][8], data["n_s 90°"].dropna(), yerr=data["n_s_error 90°"].dropna(), label=r"$n_s(90°)$", color="black", fmt="s", zorder=2)
 
 
 x_model_parallel  = data["field 0°"]/data["field 0°"][8]
@@ -175,10 +194,24 @@ x_model_parallel  = data["field 0°"]/data["field 0°"][8]
 ax.plot(x_model_parallel, model_parallel(x_model_parallel, *popt_parallel), "-b",  label=r"fit of $n_s(0°)$", zorder=3)
 
 x_model_perpendicular  = data["field 90°"].dropna()/data["field 90°"][8]
-ax.plot(x_model_perpendicular, model_perpendicular(x_model_perpendicular, *popt_perpendicular), "-g",  label=r"fit of $n_s(90°)$")
+ax.plot(x_model_perpendicular, model_perpendicular(x_model_perpendicular, *popt_perpendicular), "--g",  label=r"fit of $n_s(90°)$")
 
-
+ax.set_title(r"$\lambda_R=$" + f"{np.round(Lambda_R,2)}"
+             +r"; $\Delta=$" + f"{Delta}"
+             +r"; $\theta=$" + f"{np.round(theta,2)}"
+             + r"; $\mu$"+f"={mu}"
+             +r"; $w_0$"+f"={w_0}"
+             +r"; $L_x=$"+f"{L_x}"
+             +f"; h={h}")
 
 ax.set_xlabel(r"$B/\Delta_0$")
 ax.set_ylabel(r"$n_s$")
 ax.legend()
+
+#%% Save figure
+saving_folder = Path("/home/gabriel/Dropbox/Figures/Fitting to superfluid density")
+file_name = f'fit_lambda_R={np.round(Lambda_R,2)}_Delta={Delta}_' \
+            f'mu={mu}_L={L_x}_h={h}_B_y_in_({np.min(B_values)}-{np.round(np.max(B_values),3)})_theta={np.round(theta,2)}.png'
+
+file_path = saving_folder / file_name
+fig.savefig(file_path, dpi=300)
