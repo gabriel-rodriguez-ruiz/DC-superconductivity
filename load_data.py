6dd4ebdef23d4b6dad12e39517ef4110ed0304e4
Gabriel <gabrielrr96@gmail.com>
2025-06-19 14:52:56 -0300

    Travel to Baires.

--- load_data.py
+++ load_data.py
@@ -17,7 +17,7 @@ import os
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
-#%%
+#%% 4.9 GHz resonator
 
 data_folder = Path("Data/")
 
@@ -39,7 +39,8 @@ ax.set_ylabel(r"$n_s$")
 ax.legend()
 plt.show()
 
-#%%
+#%% 5.7 GHz resonator
+
 data_folder = Path("Data/")
 
 file_path = data_folder / 'n_s_5_7GHz.dat'
@@ -67,13 +68,17 @@ fig, ax = plt.subplots()
 
 ax.scatter(data["field 0°"][:]**2, data["n_s 0°"][:],
            label=r"$n_s(0°)$", color="red")
-a, b = np.polyfit(data["field 0°"][:8]**2,
-                              data["n_s 0°"][:8], deg=1)
+# a, b = np.polyfit(data["field 0°"][:8]**2,
+#                               data["n_s 0°"][:8], deg=1)
+a, b = np.polyfit(data["field 0°"][:15]**2,
+                              data["n_s 0°"][:15], deg=1)
 f = lambda x: a * x + b
-ax.plot(data["field 0°"][:8]**2, f(data["field 0°"][:8]**2), "b--")
+# ax.plot(data["field 0°"][:8]**2, f(data["field 0°"][:8]**2), "b--")
+ax.plot(data["field 0°"][:15]**2, f(data["field 0°"][:15]**2), "b--")
 
 ax.scatter(data["field 90°"][:]**2, data["n_s 90°"][:],
            label=r"$n_s(90°)$", color="black")
+
 m, n = np.polyfit(data["field 90°"][:15]**2,
                            data["n_s 90°"][:15], deg=1)
 h = lambda x: m * x + n
@@ -87,13 +92,71 @@ ax.legend()
 
 plt.show()
 
+#%% 9.7 GHz
+
+data_folder = Path("Data/")
+
+file_path = data_folder / 'n_s_9_7GHz.dat'
+
+data = pd.read_table(file_path, dtype=float,
+                     header=0, sep='   ',
+                     names=["field", "0°", "90°"],
+                     engine='python')
+
+fig, ax = plt.subplots()
+plt.errorbar(data["field"], data["0°"], label=r"$n_s(\gamma=\pi/2, \theta=0°)$", color="red", fmt="o")
+plt.errorbar(data["field"], data["90°"], label=r"$n_s(\gamma=\pi/2, \theta=90°)$", color="black", fmt="o")
+
+ax.set_title("9.7 GHz Resonator, 90°")
+ax.set_xlabel(r"$B$ [$T$]")
+ax.set_ylabel(r"$\frac{n_s(B)-n_s(0)}{n_s(0)}$")
+ax.legend()
+
+
+plt.show()
+
+
+#%% 5.7 GHz and 9.7 GHz resonator relative shift
+
+data_folder = Path("Data/")
+
+file_path = data_folder / 'n_s_5_7GHz.dat'
+
+data1 = pd.read_table(file_path, dtype=float,
+                     header=0, sep='\t\t',
+                     names=["field 0°", "n_s 0°", "n_s_error 0°", "field 90°",
+                            "n_s 90°", "n_s_error 90°"],
+                     engine='python')
+
+file_path = data_folder / 'n_s_9_7GHz.dat'
+
+data2 = pd.read_table(file_path, dtype=float,
+                     header=0, sep='   ',
+                     names=["field", "0°", "90°"],
+                     engine='python')
+
+fig, ax = plt.subplots()
+plt.plot(data1["field 0°"], (data1["n_s 0°"]-data1["n_s 0°"][0])/data1["n_s 0°"][0], label=r"$n_s(\gamma=0, \theta=0°)$", color="red", marker="o")
+plt.plot(data1["field 90°"], (data1["n_s 90°"]-data1["n_s 90°"][0])/data1["n_s 90°"][0], label=r"$n_s(\gamma=0, \theta=90°)$", color="black", marker="o")
+plt.plot(data2["field"], data2["0°"], label=r"$n_s(\gamma=\pi/2, \theta=0°)$", color="green", marker="o")
+plt.plot(data2["field"], data2["90°"], label=r"$n_s(\gamma=\pi/2, \theta=90°)$", color="blue", marker="o")
+
+ax.set_title("9.7 GHz Resonator, 90°")
+ax.set_xlabel(r"$B$ [$T$]")
+ax.set_ylabel(r"$\frac{n_s(B)-n_s(0)}{n_s(0)}$")
+ax.legend()
+
+
+plt.show()
+
+
 #%% Interpolation to theory
 
 data_folder = Path("../Density-of-states/Data/")
 # data_folder = Path("../anisotropic-superfluid/Data/")
 
-file_to_open = data_folder / "n_By_mu_-349.0_L=2500_h=0.001_B_y_in_(0.0-1.2)_Delta=0.2_lambda_R=1.4049144729009981_lambda_D=0_g_xx=1_g_xy=0_g_yy=1_g_yx=0_theta=1.57_points=24.npz"
-# file_to_open = data_folder / "n_By_mu_-30_L=1000_h=0.01_B_y_in_(0.0-1.2)_Delta=0.2_lambda_R=0.56_lambda_D=0_g_xx=1_g_xy=0_g_yy=1_g_yx=0_theta=1.57_points=24.npz"
+# file_to_open = data_folder / "n_By_mu_-349.0_L=2500_h=0.001_B_y_in_(0.0-1.2)_Delta=0.2_lambda_R=1.4049144729009981_lambda_D=0_g_xx=1_g_xy=0_g_yy=1_g_yx=0_theta=1.57_points=24.npz"
+file_to_open = data_folder / "n_By_mu_-349.0_L=2500_h=0.001_B_y_in_(0.0-0.48)_Delta=0.08_lambda_R=0.56_lambda_D=0_g_xx=1_g_xy=0_g_yy=1_g_yx=0_theta=1.57_points=24_beta=1000_T=True.npz"
 
 Data = np.load(file_to_open)
 
@@ -103,6 +166,7 @@ B_values = Data["B_values"]
 Lambda_R = Data["Lambda_R"]
 # Lambda_D = Data["Lambda_D"]
 Delta = Data["Delta"]
+# Delta = 0.08    # meV
 theta = Data["theta"]
 w_0 = Data["w_0"]
 mu = Data["mu"]
@@ -116,15 +180,13 @@ g_yx = Data["g_yx"]
 
 def interpolation_for_theory(x):
     return [
-            np.interp(x/1.7, B_values/Delta, n_B_y[:, 0]),        #I have change x to x/2
+            np.interp(x, B_values/Delta, n_B_y[:, 0]),        #I have change x to x/2
             np.interp(x, B_values/Delta, n_B_y[:, 1])
             ]
 
-
-
 fig, ax = plt.subplots()
-ax.plot(B_values/Delta, n_B_y[:, 0], "-sk",  label=r"$n_s(90°)$")
-ax.plot(B_values/Delta, n_B_y[:, 1], "-or",  label=r"$n_s(0°)$")
+ax.plot(B_values/Delta, n_B_y[:, 0], "sc",  label=r"$n_s(\gamma=0, \theta=90°)$")
+ax.plot(B_values/Delta, n_B_y[:, 1], "oy",  label=r"$n_s(\gamma=\pi/2, \theta=90°)$")
 ax.plot(B_values/Delta, interpolation_for_theory(B_values/Delta)[0])
 ax.plot(B_values/Delta, interpolation_for_theory(B_values/Delta)[1])
 
@@ -134,8 +196,10 @@ ax.set_title(r"$\lambda_R=$" + f"{np.round(Lambda_R,2)}"
              + r"; $\mu$"+f"={mu}"
              +r"; $w_0$"+f"={w_0}"
              +r"; $L_x=$"+f"{L_x}"
-             +f"; h={h}")
-ax.set_xlabel(r"$B$")
+             +f"; h={h}" + "\n"
+             +r"$g_{xx}=$" + f"{np.round(g_xx,2)}"
+             +r"; $g_{yy}=$" + f"{np.round(g_yy,2)}")
+ax.set_xlabel(r"$\frac{g\mu_B B}{\Delta}$")
 ax.set_ylabel(r"$n_s$")
 ax.legend()
 # saving_folder = Path("/home/gabriel/Dropbox/Figures/Fitting to superfluid density")
@@ -173,30 +237,31 @@ def model_perpendicular(x, a, c):
     # return a*(interpolation_for_theory(x)[0]) + b + c* x**2
     return a*(interpolation_for_theory(x)[0] - interpolation_for_theory(0)[0])  + data["n_s 90°"].dropna()[0] + c* x**2
 
+x_model_parallel  = data["field 0°"]/data["field 0°"][14]
+x_model_perpendicular  = data["field 90°"].dropna()/data["field 90°"][14]
+
 initial_parameters_parallel = [ 2.10435188e+06, -1.26647832e+03]
-popt_parallel, pcov_parallel = curve_fit(model_parallel, data["field 0°"]/data["field 0°"][8], data["n_s 0°"],
+popt_parallel, pcov_parallel = curve_fit(model_parallel, x_model_parallel, data["n_s 0°"],
                                           p0=initial_parameters_parallel)
 
 
 initial_parameters_perpendicular = [ 5.39729364e+06, -8.91725182e+02]
 popt_perpendicular, pcov_perpendicular = curve_fit(
-                                                   model_perpendicular, data["field 90°"].dropna()/data["field 90°"][8], data["n_s 90°"].dropna(),
+                                                   model_perpendicular, x_model_perpendicular, data["n_s 90°"].dropna(),
                                                    p0=initial_parameters_perpendicular
                                                    )
 
 fig, ax = plt.subplots()
-ax.errorbar(data["field 0°"]/data["field 0°"][8], data["n_s 0°"], yerr=data["n_s_error 0°"], label=r"$n_s(0°)$", color="red", fmt="o", zorder=1)
-ax.errorbar(data["field 90°"].dropna()/data["field 90°"][8], data["n_s 90°"].dropna(), yerr=data["n_s_error 90°"].dropna(), label=r"$n_s(90°)$", color="black", fmt="s", zorder=2)
+ax.errorbar(x_model_parallel, data["n_s 0°"], yerr=data["n_s_error 0°"], label=r"$n_s(\gamma=0, \theta=0°)$", color="red", fmt="o", zorder=1)
+ax.errorbar(x_model_perpendicular, data["n_s 90°"].dropna(), yerr=data["n_s_error 90°"].dropna(), label=r"$n_s(\gamma=0, \theta=90°)$", color="black", fmt="s", zorder=2)
 
 
-x_model_parallel  = data["field 0°"]/data["field 0°"][8]
 # fig, ax = plt.subplots()
-ax.plot(x_model_parallel, model_parallel(x_model_parallel, *popt_parallel), "-b",  label=r"fit of $n_s(0°)$", zorder=3)
+ax.plot(x_model_parallel, model_parallel(x_model_parallel, *popt_parallel), "-b",  label=r"fit of $n_s(\gamma=0, \theta=0°)$", zorder=3)
 
-x_model_perpendicular  = data["field 90°"].dropna()/data["field 90°"][8]
-ax.plot(x_model_perpendicular, model_perpendicular(x_model_perpendicular, *popt_perpendicular), "--g",  label=r"fit of $n_s(90°)$")
+ax.plot(x_model_perpendicular, model_perpendicular(x_model_perpendicular, *popt_perpendicular), "--g",  label=r"fit of $n_s(\gamma=0, \theta=90°)$")
 
-ax.set_title(r"$\lambda_R=$" + f"{np.round(Lambda_R,2)}"
+ax.set_title(r"5.7 GHz Resonator "+ r"$(\gamma=0)$"+"\n"+r"$\lambda_R=$" + f"{np.round(Lambda_R,2)}"
              +r"; $\Delta=$" + f"{Delta}"
              +r"; $\theta=$" + f"{np.round(theta,2)}"
              + r"; $\mu$"+f"={mu}"
@@ -206,7 +271,8 @@ ax.set_title(r"$\lambda_R=$" + f"{np.round(Lambda_R,2)}"
 
 ax.set_xlabel(r"$g\mu_B B/\Delta_0$")
 ax.set_ylabel(r"$n_s$")
-ax.legend()
+ax.legend(loc="upper right")
+plt.tight_layout()
 plt.show()
 #%% Save figure
 saving_folder = Path("/home/gabriel/Dropbox/Figures/Fitting to superfluid density")
@@ -218,30 +284,84 @@ fig.savefig(file_path, dpi=300)
 
 #%%
 
-g = 1
-mu_B = 5.79e-5 *1000 # meV/T
-Delta = 0.2 # meV induced gap
+B_c = data["field 0°"][14]   # 0.07  T critical field
+mu_B = 5.79e-2 # meV/T
+g = 2*0.08 / (mu_B*B_c)  #Delta/(mu_B*B_c )   #1 / 1.7
+g_xx = g
+g_yy = g
+
+B_parallel = data["field 0°"]  # T
+B_perpendicular = data["field 90°"].dropna()
+
+x_model_parallel  = data["field 0°"]/B_c
+x_model_perpendicular  = data["field 90°"].dropna()/B_c
 
 
 fig, ax = plt.subplots()
-ax.errorbar(data["field 0°"], data["n_s 0°"], yerr=data["n_s_error 0°"], label=r"$n_s(0°)$", color="red", fmt="o", zorder=1)
-ax.errorbar(data["field 90°"].dropna(), data["n_s 90°"].dropna(), yerr=data["n_s_error 90°"].dropna(), label=r"$n_s(90°)$", color="black", fmt="s", zorder=2)
+ax.errorbar(data["field 0°"], data["n_s 0°"], yerr=data["n_s_error 0°"], label=r"$n_s(\gamma=0, \theta=0°)$", color="red", fmt="o", zorder=1)
+ax.errorbar(data["field 90°"].dropna(), data["n_s 90°"].dropna(), yerr=data["n_s_error 90°"].dropna(), label=r"$n_s(\gamma=0, \theta=90°)$", color="black", fmt="s", zorder=2)
 
-B_parallel = x_model_parallel  * data["field 0°"][8]  # T
-B_perpendicular = x_model_perpendicular  * data["field 90°"][8] 
-ax.plot(B_parallel, model_parallel(x_model_parallel, *popt_parallel), "-b",  label=r"fit of $n_s(0°)$", zorder=3)
-ax.plot(B_perpendicular, model_perpendicular(x_model_perpendicular, *popt_perpendicular), "--g",  label=r"fit of $n_s(90°)$")
+initial_parameters_parallel = [ 2.10435188e+06, -1.26647832e+03]
+popt_parallel, pcov_parallel = curve_fit(model_parallel, x_model_parallel, data["n_s 0°"],
+                                          p0=initial_parameters_parallel)
 
-ax.set_title(r"$\lambda_R=$" + f"{np.round(Lambda_R,2)}meV"
-             +r"; $\Delta=$" + f"{Delta}meV"
+initial_parameters_perpendicular = [ 3.73583079e+06, -5.40525133e+02]
+popt_perpendicular, pcov_perpendicular = curve_fit(
+                                                   model_perpendicular, x_model_perpendicular, data["n_s 90°"].dropna(),
+                                                   p0=initial_parameters_perpendicular
+                                                   )
+
+standard_deviation_parallel = np.sqrt(np.diag(pcov_parallel))
+standard_deviation_perpendicular = np.sqrt(np.diag(pcov_perpendicular))
+
+ax.plot(B_parallel, model_parallel(x_model_parallel, *popt_parallel), "-b",  label=r"fit of $n_s(\gamma=0, \theta=0°)$", zorder=3)
+ax.plot(B_perpendicular, model_perpendicular(x_model_perpendicular, *popt_perpendicular), "--g",  label=r"fit of $n_s(\gamma=0, \theta=90°)$")
+
+ax.set_title(r"5.7 GHz Resonator "+ r"$(\gamma=0)$"+"\n"+r"$\lambda_R=$" + f"{np.round(Lambda_R,2)}"
+             +r"; $\Delta=$" + f"{Delta}"
              +r"; $\theta=$" + f"{np.round(theta,2)}"
-             + r"; $\mu$"+f"={mu}meV"+ "\n"
-             +r"$w_0$"+f"={w_0}meV"
+             + r"; $\mu$"+f"={mu}"
+             +r"; $w_0$"+f"={w_0}"
              +r"; $L_x=$"+f"{L_x}"
-             +f"; h={h}"
-             + r"; $g_{xx}/g_{yy}=0.6$")
+             +f"; h={h}"+"\n"
+             +r"$g_{xx}=$" + f"{np.round(g_xx,
+             2)}"
+             +r"; $g_{yy}=$" + f"{np.round(g_yy,2)}"
+             + r"; $T=0$")
 
 ax.set_xlabel(r"$B(T)$")
 ax.set_ylabel(r"$n_s$")
-ax.legend()
+ax.legend(loc="upper right")
+plt.tight_layout()
 plt.show()
+
+#%% Save txt
+
+data_folder = Path("Files/")
+
+name = "Fitting_5.7GHz.txt"
+file_to_open = data_folder / name
+
+data["fit 0°"] = model_parallel(x_model_parallel, *popt_parallel)
+data["fit 90°"] = model_perpendicular(x_model_perpendicular, *popt_perpendicular)
+data.to_csv(file_to_open, sep='\t', header=True)
+
+#%% 
+
+data_folder = Path("Files/")
+
+name = "Fitting_5.7GHz.txt"
+file_to_open = data_folder / name
+
+Data2 = pd.read_table(file_to_open, dtype=float,
+                     header=0, sep='\t',
+                     names=["field 0°", "n_s 0°", "n_s_error 0°", "field 90°",
+                            "n_s 90°", "n_s_error 90°", "fit 0°", "fit 90°"],
+                     engine='python')
+
+fig, ax = plt.subplots()
+
+ax.plot(Data2["field 0°"], Data["n_s 0°"])
+ax.plot(Data2["field 90°"], Data["n_s 90°"])
+ax.plot(Data2["field 0°"], Data["fit 0°"])
+ax.plot(Data2["field 90°"], Data["fit 90°"])
--- load_data_angle.py
+++ load_data_angle.py
@@ -52,7 +52,7 @@ plt.show()
 
 data_folder = Path("../anisotropic-superfluid/Data/")
 
-file_to_open = data_folder / "n_theta_mu_-349.0_L=2500_h=0.001_theta_in_(0.0-1.571)B=0.29_Delta=0.2_lambda_R=1.4049144729009981_lambda_D=0_g_xx=1_g_xy=0_g_yy=1.1_g_yx=0_points=16.npz"
+file_to_open = data_folder / "n_theta_mu_-349.0_L=2500_h=0.001_theta_in_(0.0-1.571)B=0.29_Delta=0.2_lambda_R=1.4049144729009981_lambda_D=0_g_xx=1_g_xy=0_g_yy=1_g_yx=0_points=16.npz"
 
 Data = np.load(file_to_open)
 
@@ -96,17 +96,15 @@ def interpolation_for_theory(x):
             np.interp(x, theta_values, n_theta_45[:, 2])            # 45°
             ]
 
+# fig, ax = plt.subplots()
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 
-
-fig, ax = plt.subplots()
-# fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-
-# ax.plot(theta_values, n_theta_0_90[:, 0], "-sk",  label=r"$n_s(0°)$")
-ax.plot(theta_values, n_theta_0_90[:, 1], "-or",  label=r"$n_s(90°)$")
+ax.plot(theta_values, n_theta_0_90[:, 0], "-or",  label=r"$n_s(0°)$")
+# ax.plot(theta_values, n_theta_0_90[:, 1], "sk",  label=r"$n_s(90°)$")
 # ax.plot(theta_values, n_theta_45[:, 2], "-*g",  label=r"$n_s(45°)$")
 
-# ax.plot(theta_values, interpolation_for_theory(theta_values)[0])    # 0°
-ax.plot(theta_values, interpolation_for_theory(theta_values)[1])    # 90°
+ax.plot(theta_values, interpolation_for_theory(theta_values)[0])    # 0°
+# ax.plot(theta_values, interpolation_for_theory(theta_values)[1])    # 90°
 # ax.plot(theta_values, interpolation_for_theory(theta_values)[2])    # 45°
 
 
@@ -157,29 +155,31 @@ popt_perpendicular, pcov_perpendicular = curve_fit(
                                                    )
 
 initial_parameters_45 = [ 5.39729364e+06, 2.5e7]
-popt_45, pcov_45 = curve_fit(
-                                                   model_45, angle, data["n_s 45°"],
+popt_45, pcov_45 = curve_fit(model_45, angle, data["n_s 45°"],
                                                    p0=initial_parameters_45
                                                    )
 
+standard_deviation_parallel = np.sqrt(np.diag(pcov_parallel))
+standard_deviation_perpendicular = np.sqrt(np.diag(pcov_perpendicular))
+standard_deviation_45 = np.sqrt(np.diag(pcov_45))
 
 fig, ax = plt.subplots()
 # fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 
 
-ax.errorbar(angle, data["n_s 0°"], yerr=data["n_s_error 0°"], label=r"$n_s(0°)$", color="red", fmt="o", zorder=1)
+# ax.errorbar(angle, data["n_s 0°"], yerr=data["n_s_error 0°"], label=r"$n_s(0°)$", color="red", fmt="o", zorder=1)
 # ax.errorbar(angle, data["n_s 90°"], yerr=data["n_s_error 90°"], label=r"$n_s(90°)$", color="black", fmt="s", zorder=2)
-# ax.errorbar(angle, data["n_s 45°"], yerr=data["n_s_error 45°"], label=r"$n_s(45°)$", color="blue", fmt="*", zorder=2)
+ax.errorbar(angle, data["n_s 45°"], yerr=data["n_s_error 45°"], label=r"$n_s(45°)$", color="blue", fmt="*", zorder=2)
 
 
 x_model_parallel  = theta_values
-ax.plot(x_model_parallel, model_parallel(x_model_parallel, *popt_parallel), "-b",  label=r"fit of $n_s(0°)$", zorder=3)
+# ax.plot(x_model_parallel, model_parallel(x_model_parallel, *popt_parallel), "-b",  label=r"fit of $n_s(0°)$", zorder=3)
 
 x_model_perpendicular  = theta_values
 # ax.plot(x_model_perpendicular, model_perpendicular(x_model_perpendicular, *popt_perpendicular), "-g",  label=r"fit of $n_s(90°)$")
 
 x_model_45  = theta_values
-# ax.plot(x_model_45, model_45(x_model_45, *popt_45), "-r",  label=r"fit of $n_s(45°)$")
+ax.plot(x_model_45, model_45(x_model_45, *popt_45), "-r",  label=r"fit of $n_s(45°)$")
 
 
 ax.set_title(r"$\lambda_R=$" + f"{np.round(Lambda_R,2)}"
@@ -188,14 +188,69 @@ ax.set_title(r"$\lambda_R=$" + f"{np.round(Lambda_R,2)}"
              + r"; $\mu$"+f"={mu}"
              +r"; $w_0$"+f"={w_0}"
              +r"; $L_x=$"+f"{L_x}"
-             +f"; h={h}")
+             +f"; h={h}"
+             +r"; $g_{xx}=$" + f"{g_xx}"
+             +r"; $g_{yy}=$" + f"{g_yy}")
 
-ax.set_ylim([2.12e7, 2.14e7])  # n_s 0°
+# ax.set_ylim([2.12e7, 2.14e7])  # n_s 0°
 # ax.set_ylim([2.72e7, 2.73e7])    # n_s 90°
-# ax.set_ylim([2.97e7, 3e7])    # n_s 45°
+ax.set_ylim([2.97e7, 3e7])    # n_s 45°
 # ax.set_ylim([2.1e7, 2.73e7])    # n_s 90° and 0°
+
+# def geometric_factor(x):
+#     g_xx = 1
+#     g_yy = 1
+#     a = min(data["n_s 0°"])
+#     b = max(data["n_s 0°"])
+#     g = np.sqrt((g_xx*np.cos(x))**2 + (g_yy*np.sin(x))**2)
+#     return np.array([a*(g_yy*np.sin(x)/g)**2 + b*(g_xx*np.cos(x)/g)**2,
+#                      a*(g_xx*np.sin(x)/g)**2 + b*(g_yy*np.cos(x)/g)**2,
+#                      None
+#                      ])
+# ax.plot(theta_values, [geometric_factor(theta)[1] for theta in theta_values])
+
 
 ax.set_xlabel(r"$\theta$")
 ax.set_ylabel(r"$n_s$")
+plt.tight_layout()
 ax.legend()
 plt.show()
+
+
+#%% Save txt
+
+data_folder = Path("Files/")
+
+name = "n_s_50_mT_rotation.txt"
+file_to_open = data_folder / name
+
+# data["fit n_s 0°"] = model_parallel(x_model_parallel, *popt_parallel)
+# data["fit n_s 90°"] = model_perpendicular(x_model_perpendicular, *popt_perpendicular)
+# data["fit n_s 45°"] = model_45(x_model_45, *popt_45)
+data["fit n_s 0°"] = model_parallel(angle, *popt_parallel)
+data["fit n_s 90°"] = model_perpendicular(angle, *popt_perpendicular)
+data["fit n_s 45°"] = model_45(angle, *popt_45)
+
+data.to_csv(file_to_open, sep='\t', header=True, index=False)
+
+#%%
+data_folder = Path("Files/")
+
+name = "n_s_50_mT_rotation.txt"
+file_to_open = data_folder / name
+
+Data = pd.read_table(file_to_open, dtype=float,
+                     header=0, sep='\t',
+                     names=['angle [°]', 'n_s 0°', 'n_s_error 0°', 'n_s 45°', 'n_s_error 45°',
+                            'n_s 90°', 'n_s_error 90°', 'fit n_s 0°', 'fit n_s 90°', 'fit n_s 45°'],
+                     engine='python')
+
+fig, ax = plt.subplots()
+
+ax.plot(Data['angle [°]'], Data["n_s 0°"])
+ax.plot(Data['angle [°]'], Data["n_s 90°"])
+ax.plot(Data['angle [°]'], Data["n_s 45°"])
+ax.plot(Data['angle [°]'], Data["fit n_s 0°"])
+ax.plot(Data['angle [°]'], Data["fit n_s 90°"])
+ax.plot(Data['angle [°]'], Data["fit n_s 45°"])
+
